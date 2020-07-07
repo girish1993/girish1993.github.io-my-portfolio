ee9c6c027514ab7d0afc3bfe120d690cf2872fe3
girish.bhatta <girish.bhatta@alexsolutions.com.au>
2020-07-07 23:00:59 +1000

    Updates

---
+++ precache-manifest.ac49b42e637306cc439142189c392da5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d85bbda0c1e15200c9c1a1a30e719e6",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "8d69442819a192c7ffac",
+    "url": "/my-portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "69b5035f4376fe52c553",
+    "url": "/my-portfolio/static/css/main.2f047c18.chunk.css"
+  },
+  {
+    "revision": "8d69442819a192c7ffac",
+    "url": "/my-portfolio/static/js/2.5291971d.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/my-portfolio/static/js/2.5291971d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69b5035f4376fe52c553",
+    "url": "/my-portfolio/static/js/main.05cc8082.chunk.js"
+  },
+  {
+    "revision": "f241adf7bba56ceeeb82",
+    "url": "/my-portfolio/static/js/runtime-main.8b00eace.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-portfolio/precache-manifest.84a421a98d6499ef2a705b7767229c54.js"
+  "/my-portfolio/precache-manifest.ac49b42e637306cc439142189c392da5.js"
 );
 
 self.addEventListener('message', (event) => {
