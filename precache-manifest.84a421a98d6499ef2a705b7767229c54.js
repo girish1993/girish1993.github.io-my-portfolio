fa0ca2c12f41d1d77059f81356e6dd37aeb9f714
girish.bhatta <girish.bhatta@alexsolutions.com.au>
2020-07-07 20:39:48 +1000

    Updates

---
+++ precache-manifest.84a421a98d6499ef2a705b7767229c54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65adf78954ded97cf5e4c22357e495a5",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "b891ee1525e1b4a9feed",
+    "url": "/my-portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "982f8db5562c50b0d968",
+    "url": "/my-portfolio/static/js/2.c7663357.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-portfolio/static/js/2.c7663357.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b891ee1525e1b4a9feed",
+    "url": "/my-portfolio/static/js/main.22c76035.chunk.js"
+  },
+  {
+    "revision": "f241adf7bba56ceeeb82",
+    "url": "/my-portfolio/static/js/runtime-main.8b00eace.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
