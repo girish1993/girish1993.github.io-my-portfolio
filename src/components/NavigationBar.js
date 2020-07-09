8b19ae47001301bdc176aabec20aadfc05df0657
girish.bhatta <girish.bhatta@alexsolutions.com.au>
2020-07-09 23:20:00 +1000

    completing the header layout

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,27 +1,25 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import MenuItem from './MenuItem';
+
 
 function NavigationBar() { 
     return (
-        <Navbar  expand="lg">
-            <Navbar.Brand href="#home">Girish Bhatta</Navbar.Brand>
-            <Nav className="justify-content-end" activeKey="/home">
-                <Nav.Item>
-                    <Nav.Link href="/home">Home</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link eventKey="link-1">About Me</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link eventKey="link-2">Portfolio</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link eventKey="link-3">Blogs</Nav.Link>
-                </Nav.Item>
+        <Navbar bg="dark" variant="dark" expand="lg">
+            <div style = {{float:"left", width:"70%"}}>
+                <Navbar.Brand style={{fontSize:"23px"}}href="#home">Girish Bhatta</Navbar.Brand>
+            </div>
+        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+        <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav" style={{float:"right",width:"30%"}}>
+            <Nav className="mr-auto">
+                <MenuItem itemDetails = {{link:"/home",name:"Home"}} />
+                <MenuItem itemDetails = {{link:"#link",name:"About Me"}} />
+                <MenuItem itemDetails = {{link:"#link",name:"Projects"}} />
+                <MenuItem itemDetails = {{link:"#link",name:"Blog"}} />
             </Nav>
+        </Navbar.Collapse>
         </Navbar>
-
     );
 }
 
