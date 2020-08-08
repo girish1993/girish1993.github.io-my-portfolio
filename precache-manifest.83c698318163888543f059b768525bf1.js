0f0f60aa3baaa4a297c3b5dbe9bc2a9822918fbd
girish.bhatta <girish.bhatta@alexsolutions.com.au>
2020-08-09 00:10:17 +1000

    Updates

--- precache-manifest.83c698318163888543f059b768525bf1.js
+++ precache-manifest.83c698318163888543f059b768525bf1.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3f55e0f8b8b8d212503d87317122f9c3",
+    "revision": "3b2fbda921237fdafdc90becc13e80a2",
     "url": "/my-portfolio/index.html"
   },
   {
-    "revision": "82f3b59c154dc49c4cde",
+    "revision": "56ee35189f7754670209",
     "url": "/my-portfolio/static/css/2.af3c1da9.chunk.css"
   },
   {
-    "revision": "33aee3c04e7c3c3f2f83",
+    "revision": "ce7408ae7d076a94f756",
     "url": "/my-portfolio/static/css/main.2fb2dce6.chunk.css"
   },
   {
-    "revision": "82f3b59c154dc49c4cde",
-    "url": "/my-portfolio/static/js/2.4a575d23.chunk.js"
+    "revision": "56ee35189f7754670209",
+    "url": "/my-portfolio/static/js/2.db9719dd.chunk.js"
   },
   {
     "revision": "5e9b6f5c16b720e8a86875f97734673e",
-    "url": "/my-portfolio/static/js/2.4a575d23.chunk.js.LICENSE.txt"
+    "url": "/my-portfolio/static/js/2.db9719dd.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "33aee3c04e7c3c3f2f83",
-    "url": "/my-portfolio/static/js/main.cf84cc72.chunk.js"
+    "revision": "ce7408ae7d076a94f756",
+    "url": "/my-portfolio/static/js/main.c02d43d7.chunk.js"
   },
   {
     "revision": "f241adf7bba56ceeeb82",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-portfolio/precache-manifest.39cfdc4c5669865d727d9b1f42325790.js"
+  "/my-portfolio/precache-manifest.83c698318163888543f059b768525bf1.js"
 );
 
 self.addEventListener('message', (event) => {
